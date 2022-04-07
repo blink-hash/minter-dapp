f36a11ba1cba9b9cba20b870bd76460e118cf100
Blink-hash <preya12@gmail.com>
2022-04-08 00:09:27 +0100

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xeBf23Fd9f01DB4B69A5C6Cb91F8813736b62e134"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
